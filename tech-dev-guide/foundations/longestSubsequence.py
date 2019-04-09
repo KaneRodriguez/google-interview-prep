bc8c9eb660e516cde4d7a0a6ed2e2d9358202431
Kane Rodriguez <kaustez@siu.edu>
2019-04-09 13:51:59 -0500

    Update longestSubsequence.py

--- tech-dev-guide/foundations/longestSubsequence.py
+++ tech-dev-guide/foundations/longestSubsequence.py
@@ -1,4 +1,7 @@
-# TODO: read hints and solution given on page, implement
+# This is the O(s + d*w) solution. Where:
+# s is the lenght of input string S 
+# d is the number of words in the dictionary/list 
+# w is the average length of the words (or max length for a looser upper bound)
 
 # https://techdevguide.withgoogle.com/paths/foundational/find-longest-word-in-dictionary-that-subsequence-of-given-string#!
 def longestSub(S,D):
@@ -25,4 +28,5 @@ def longestSub(S,D):
       
   return longestWord
       
-print(longestSub("abppple", ["able", "apple", "ale", "z"]))      
+print(longestSub("wabppple", ["able", "apple", "ale", "z"]))      
+print(longestSub("hellog1e2e3k4sforg55eeks", ["pintu", "geeksfor", "geeksgeek", " forgeek", "llog1e2e3"]))      
